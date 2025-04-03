e4a95bed9542f6b4f8278b9b55c99adc9658e695
DonatasNoreika <donoras@gmail.com>
2025-04-03 19:45:24 +0300

    Pakeičiau pradinį puslapį į index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import numpy as np
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     print(np.sqrt(25))
-    return 'Hello, World!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
